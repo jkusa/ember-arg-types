5d8ca7e17a1a29f20fdb41befab064dbaadeb880
Jon Kilroy <kilroy@verizonmedia.com>
2020-03-26 18:05:05 -0500

    Tell ember-auto-import to ignore glimmer import

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+  options: {
+    autoImport: {
+      exclude: ['@glimmer/component']
+    }
+  }
 };
